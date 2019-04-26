4ee7181286a14a96fee52dbfe47ff34b3edc989e
Sara <sarahlynnep@hotmail.com>
2019-04-26 14:39:02 +0200

    Updated the classifier comparison code to show ranking of best grid search results

--- sarah/compare_classifiers.py
+++ sarah/compare_classifiers.py
@@ -1,9 +1,11 @@
 import data_loader
+import warnings
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
+from sklearn.exceptions import ConvergenceWarning
 from numpy import mean, std # for mean and standard deviations of match performance
 
 # Try running gridsearch for all of the classifiers!
@@ -23,8 +25,7 @@ def prepareGridSearchForSVC():
 def prepareGridSearchForLRC():
     classifier = LogisticRegression(multi_class='auto')
     parameters = {
-        'solver' : ('liblinear', 'newton-cg', 'lbfgs'),
-        #'solver' : ('liblinear', 'newton-cg', 'sag', 'saga', 'lbfgs'),
+        'solver' : ('liblinear', 'newton-cg', 'sag', 'saga', 'lbfgs'),
         'C' : (0.1, 1, 10, 100, 1000) }
 
     return createGridSearchCV(classifier, parameters)
@@ -44,7 +45,7 @@ def prepareGridSearchForMLP():
     classifier = MLPClassifier()
     parameters = {
         'hidden_layer_sizes': [(10, 10), (30, 30, 30), (100, 100), (100, 100, 100)],
-        #'solver' : ('adam', 'lbfgs', 'sgd'),
+        #'solver' : ('adam', 'lbfgs', 'sgd'),       # this makes the run time very long (70+ mins)
         'tol': (1e-2, 1e-3, 1e-4, 1e-5, 1e-6),
         'epsilon': (1e-3, 1e-7, 1e-8, 1e-9, 1e-8)
     }
@@ -57,35 +58,43 @@ def runGridSearch(dataset, target, name, classifier):
     classifier.fit(dataset[:70], target[:70])
     test_score = classifier.score(dataset[70:100], target[70:100])
 
-    #print(classifier.cv_results_['mean_test_score'])
-    #print(classifier.cv_results_['std_test_score'])
+    print(classifier.cv_results_['mean_test_score'])
+    print(classifier.cv_results_['std_test_score'])
+
+    print("Best training result: {} (index: {})".format(classifier.best_score_, classifier.best_index_))
+    print("---------------------------------------------------")
 
     return {'name' : name,
             'cv_results' : classifier.cv_results_,
             'classifier' : classifier.best_estimator_,
             'parameters' : classifier.best_params_,
-            'test_score' : test_score }
+            'max_training_score' : classifier.best_score_,
+            'test_score' : test_score}
 
 def compareClassifiers(dataset, target):
     results = []
 
-    SVC_results = runGridSearch(dataset, target, "SupportVectorClassifier", prepareGridSearchForSVC())
-    results.append(SVC_results)
+    # To get rid of ConvergenceWarnings
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
-    RFC_results = runGridSearch(dataset, target, "RandomForestClassifier", prepareGridSearchForRFC())
-    results.append(RFC_results)
+        SVC_results = runGridSearch(dataset, target, "SupportVectorClassifier", prepareGridSearchForSVC())
+        results.append(SVC_results)
 
-    LRC_results = runGridSearch(dataset, target, "LogisticRegressionClassifier", prepareGridSearchForLRC())
-    results.append(LRC_results)
+        RFC_results = runGridSearch(dataset, target, "RandomForestClassifier", prepareGridSearchForRFC())
+        results.append(RFC_results)
 
-    MLPC_results = runGridSearch(dataset, target, "MultiLayerPerceptronClassifier", prepareGridSearchForMLP())
-    results.append(MLPC_results)
+        LRC_results = runGridSearch(dataset, target, "LogisticRegressionClassifier", prepareGridSearchForLRC())
+        results.append(LRC_results)
 
-    # return result with the highest test score
-    ranked_results = sorted(results, key=lambda result: result['test_score'])
+        MLPC_results = runGridSearch(dataset, target, "MultiLayerPerceptronClassifier", prepareGridSearchForMLP())
+        results.append(MLPC_results)
 
-    for index, result in enumerate(ranked_results):
-        print("#{}: {} (test_score: {}) with parameters {}", index, result['name'], result['test_score'], result['parameters'])
+    # return result with the highest test score first
+    ranked_results = sorted(results, key=lambda result: result['test_score'], reverse=True)
+
+    for index, result in enumerate(ranked_results, start=1):
+        print("#{}: {} (test_score: {}) with parameters {}".format(index, result['name'], result['test_score'], result['parameters']))
 
     return ranked_results[0]
 
@@ -99,3 +108,4 @@ if __name__ == "__main__":
     print("---------------------------------------------------")
     print("Optimal classifier: {} with parameters {}".format(result['name'], result['parameters']))
     print("Test score:", result['test_score'])
+    print("---------------------------------------------------")
